ddc477d323f0ed0a714ca14f187845ecb7c6ca73
Sergei Dryganets <sergeyd@microsoft.com>
2018-01-26 17:09:53 -0800

    Tests are datadriven. Tests use the new Regexp implemnetation

--- src/RegExpI18n.ts
+++ src/RegExpI18n.ts
@@ -1,6 +1,8 @@
 
-export class RegExpI18n extends RegExp {
+class RegExpI18n extends RegExp {
     constructor(regexp: string, flags?: string) {
         super(regexp, flags);
     }
 }
+
+export = RegExpI18n;
--- test/RegExpI18n.spec.ts
+++ test/RegExpI18n.spec.ts
@@ -1,6 +1,23 @@
+import RegExpI18n = require('../src/RegExpI18n');
 
-describe('Latin', function () {
-    it('Latin', () => {
-        //
+interface Spec {
+    language: string;
+    testText;
+}
+
+// Every spec represens separate langauge
+const specs: Spec[] = [
+    {language: 'Latin', testText: 'abcZYX'}
+];
+
+// replace all not letter characters
+const testRegexp = new RegExpI18n('^[\\pL]/gi');
+
+describe('SmokeTests', function () {
+    specs.forEach(spec => {
+        it(spec.language, () => {
+            expect(spec.testText.replace(testRegexp, '')).toBe(spec.testText);
+        });
     });
-};
+    
+});
