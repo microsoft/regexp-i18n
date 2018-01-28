46be72e48fd6114af550e68e071ea0c0e640b568
Sergei Dryganets <sergeyd@microsoft.com>
2018-01-28 11:07:59 -0800

    regexp table generation rewritten in python3

--- tools/all-alpha-range.py
+++ tools/all-alpha-range.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 def code(val):
   code =  hex(val)[2:]
   while (len(code) != 4):
     code = '0' + code
-  return code
+  return '\\u' + code
 
 result = ''
 max = pow(2, 16)
 i = 0;
 first_alpha = -1
 last_alpha = -1
-cur_char = unichr(0)
+cur_char = chr(0)
 while(i < max):
   while(i < max):
-    cur_char = unichr(i)
+    cur_char = chr(i)
     if (cur_char.isalpha()):
       first_alpha = last_alpha  = i
       break
@@ -22,14 +22,13 @@ while(i < max):
   
   while(i < max):
     i+=1
-    cur_char = unichr(i)
+    cur_char = chr(i)
     if (not cur_char.isalpha()):
       last_alpha = i-1
       if last_alpha != first_alpha:
-        result += '\\u' + code(first_alpha) + '-' + '\\u' + code(last_alpha)
+        result +=  code(first_alpha) + '-' + code(last_alpha)
       else:
-        result += '\\u'+code(first_alpha)
+        result += code(first_alpha)
       break
 
 print(result)
-
