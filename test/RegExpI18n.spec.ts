56625750616233b7e0f704679f268c19e217022c
Sergei Dryganets <sergeyd@microsoft.com>
2018-01-30 11:06:59 -0800

    Tests reorgonized. Added testcase for the regexp we intend to use

--- test/RegExpI18n.spec.ts
+++ test/RegExpI18n.spec.ts
@@ -1,5 +1,4 @@
-//import RegExpI18n = require('../src/RegExpI18n');
-import { XRegExp } from 'xregexp';
+import _ = require('lodash');
 
 import { LETTERS } from '../src/RegExpI18n';
 
@@ -21,16 +20,59 @@ interface TestCase {
     tearDown?: () => void;
 }
 
-// Every spec represens separate langauge
-// Languages are ordered in the usage order
+const testData: Spec[] = [
+    { name: 'Latin', testText: 'dabcZYX' },
+    { name: 'Latin suplemental', testText: 'ÀÊÖØåöøüÿ'  },
+    { name: 'Chinese', testText: '他走過城市的狗他的兄弟生氣了' },
+    { name: 'Chinese Simplified', testText: '渔夫从远处看见一位渔夫罕见的字符' },
+    { name: 'Arabic', testText: 'العربية' },  // 660M
+    { name: 'Devenagari', testText: 'देवनागरीलिपि' }, //600M+
+    { name: 'Eastern Nagari', testText: 'পৰবনগৰ' }, //300M+
+    { name: 'Cyrillic', testText: 'НочьУлицаФонарьАптекаЃё' }, //250M+
+    { name: 'Kana', testText: 'かなカナウィキペディア日本語版' }, //120M+   
+    { name: 'Javanese', testText: 'ꦗꦮ' }, //80M+    
+    { name: 'Hangul', testText: '한글조선글' }, //80M+    
+    { name: 'Telugu', testText: 'తెలుగు' }, //74M+
+    //{ name: 'Tamil', testText: 'நன்னூல்' }, //60M+
+    { name: 'Gujarati', testText: 'ગુજરાતી' }, //48M+
+    //{ name: 'Kannada', testText: 'ಕನ್ನಡ' }, // 45M+
+    //{ name: 'Burmese', testText: 'မြန်မာ' }, //39M+
+    { name: 'Malayalam', testText: 'മലയാളം' }, //38M+
+    { name: 'Thai', testText: 'ไทย' }, //38M+
+    //{ name: 'Sundanese', testText: 'ᮞᮥᮔ᮪ᮓ' },
+    { name: 'Gurmukhi', testText: 'ਗੁਰਮੁਖੀ' }, //22M+
+    { name: 'Lao', testText: 'ລາວ' }, // 22M+
+    //{ name: 'Odia', testText: 'ଉତ୍କଳ' }, // 21M+
+    { name: 'Ge\'ez', testText: 'ግዕዝ' },  // 18M
+    { name: 'Sinhalese', testText: 'සිංහල' }, // 14.4M
+    { name: 'Hebrew', testText: 'אלפבית' }, // 14M
+    { name: 'Armenian', testText: 'Հայոց' }, //12M
+    //{ name: 'Khmer', testText: 'ខ្មែរ' }, // 11.4M
+    { name: 'Greek', testText: 'Ελληνικό' }, // 11M
+    //{ name: 'Batak', testText: 'ᯅᯖᯂ᯲ᯆᯗᯂ᯳ᯅᯖᯃ᯳ᯅᯗᯂ᯲ᯅᯖᯄᯱ᯲' }, //8.5M
+    { name: 'Lontara', testText: 'ᨒᨚᨈᨑ' }, // 5.6M
+    { name: 'Balinese', testText: 'ᬩᬮᬶ' }, // 6M
+    //{ name: 'Tibetan', testText: 'བོད་' }, // 5M
+    { name: 'Georgian', testText: 'ქართული' }, // 4.5M
+    { name: 'Modern Yi', testText: 'ꆈꌠ' }, // 4M
+    { name: 'Mongolian', testText: 'ᠮᠣᠩᠭᠣᠯ' }, // 2M
+    { name: 'Tifinagh', testText: 'ⵜⵉⴼⵉⵏⴰⵖ' }, // 1M
+    { name: 'Tai Le', testText: 'ᥖᥭᥰᥘᥫᥴ' }, // 0.75M
+    { name: 'New Tai Lue', testText: 'ᦑᦟᦹᧉ' }, // 0.55M
+    { name: 'Syriac', testText: 'ܣܘܪܝܬ' }, // 0.4M
+    { name: 'Thaana', testText: 'ދިވެހި' }, // 
+    { name: 'Inuktitut', testText: 'ᐃᓄᒃᑎᑐᑦ' }, //0.035M
+    { name: 'Cherokee', testText: 'ᏣᎳᎩ' },  // 0.02M
+    { name: 'Hanunó\'o', testText: 'ᜱᜨᜳᜨᜳᜢ' }, // 0.013M*/
+
+];
 
 const testCases: TestCase[] = [
     {
         name: 'SmokeTests',
         // we replace all non letter characters
         regexp: new RegExp('[^' + LETTERS + ']', 'gu'),
-        // Test case should contain letters only
-        //preConditionRegexp: XRegExp('\\\\pL+', 'g'),
+        
         testFunction: function (spec: Spec) {
             const actual = spec.testText.replace(this.regexp, '');
             const expected = spec.expected ? spec.expected : spec.testText;
@@ -40,58 +82,39 @@ const testCases: TestCase[] = [
 
         setup: function() {
             this.tests.forEach(test => {
-                test.expected = test.testText.normalize();
+                test.expected = test.testText;
                 test.testText = 1 + test.testText + 2;
             });
         },
 
-        tests: [
-            { name: 'Latin', testText: 'dabcZYX' },
-            { name: 'Latin suplemental', testText: 'ÀÊÖØåöøüÿ'  },
-            { name: 'Chinese', testText: '他走過城市的狗他的兄弟生氣了' },
-            { name: 'Chinese Simplified', testText: '渔夫从远处看见一位渔夫罕见的字符' },
-            { name: 'Arabic', testText: 'العربية' },  // 660M
-            { name: 'Devenagari', testText: 'देवनागरीलिपि' }, //600M+
-            { name: 'Eastern Nagari', testText: 'পৰবনগৰ' }, //300M+
-            { name: 'Cyrillic', testText: 'НочьУлицаФонарьАптекаЃё' }, //250M+
-            { name: 'Kana', testText: 'かなカナウィキペディア日本語版' }, //120M+   
-            { name: 'Javanese', testText: 'ꦗꦮ' }, //80M+    
-            { name: 'Hangul', testText: '한글조선글' }, //80M+    
-            { name: 'Telugu', testText: 'తెలుగు' }, //74M+
-            //{ name: 'Tamil', testText: 'நன்னூல்' }, //60M+
-            { name: 'Gujarati', testText: 'ગુજરાતી' }, //48M+
-            //{ name: 'Kannada', testText: 'ಕನ್ನಡ' }, // 45M+
-            //{ name: 'Burmese', testText: 'မြန်မာ' }, //39M+
-            { name: 'Malayalam', testText: 'മലയാളം' }, //38M+
-            { name: 'Thai', testText: 'ไทย' }, //38M+
-            //{ name: 'Sundanese', testText: 'ᮞᮥᮔ᮪ᮓ' },
-            { name: 'Gurmukhi', testText: 'ਗੁਰਮੁਖੀ' }, //22M+
-            { name: 'Lao', testText: 'ລາວ' }, // 22M+
-            //{ name: 'Odia', testText: 'ଉତ୍କଳ' }, // 21M+
-            { name: 'Ge\'ez', testText: 'ግዕዝ' },  // 18M
-            { name: 'Sinhalese', testText: 'සිංහල' }, // 14.4M
-            { name: 'Hebrew', testText: 'אלפבית' }, // 14M
-            { name: 'Armenian', testText: 'Հայոց' }, //12M
-            //{ name: 'Khmer', testText: 'ខ្មែរ' }, // 11.4M
-            { name: 'Greek', testText: 'Ελληνικό' }, // 11M
-            //{ name: 'Batak', testText: 'ᯅᯖᯂ᯲ᯆᯗᯂ᯳ᯅᯖᯃ᯳ᯅᯗᯂ᯲ᯅᯖᯄᯱ᯲' }, //8.5M
-            { name: 'Lontara', testText: 'ᨒᨚᨈᨑ' }, // 5.6M
-            { name: 'Balinese', testText: 'ᬩᬮᬶ' }, // 6M
-            //{ name: 'Tibetan', testText: 'བོད་' }, // 5M
-            { name: 'Georgian', testText: 'ქართული' }, // 4.5M
-            { name: 'Modern Yi', testText: 'ꆈꌠ' }, // 4M
-            { name: 'Mongolian', testText: 'ᠮᠣᠩᠭᠣᠯ' }, // 2M
-            { name: 'Tifinagh', testText: 'ⵜⵉⴼⵉⵏⴰⵖ' }, // 1M
-            { name: 'Tai Le', testText: 'ᥖᥭᥰᥘᥫᥴ' }, // 0.75M
-            { name: 'New Tai Lue', testText: 'ᦑᦟᦹᧉ' }, // 0.55M
-            { name: 'Syriac', testText: 'ܣܘܪܝܬ' }, // 0.4M
-            { name: 'Thaana', testText: 'ދިވެހި' }, // 
-            { name: 'Inuktitut', testText: 'ᐃᓄᒃᑎᑐᑦ' }, //0.035M
-            { name: 'Cherokee', testText: 'ᏣᎳᎩ' },  // 0.02M
-            { name: 'Hanunó\'o', testText: 'ᜱᜨᜳᜨᜳᜢ' }, // 0.013M*/
-            
-        ]
+        tests: _.cloneDeep(testData)
     },
+
+    {
+        name: 'StripNotAlphaDigit',        
+        regexp: new RegExp('[^\\d' + LETTERS + ']+$|^[^\\d' + LETTERS + ']+', 'gu'),
+
+        testFunction: function (spec: Spec) {
+            const actual = spec.testText.replace(this.regexp, '');
+            const expected = spec.expected ? spec.expected : spec.testText;
+
+            expect(actual).toBe(expected);
+        },
+
+        setup: function() {
+            this.tests.forEach(test => {
+
+                const mid = test.testText.length / 2;
+                // adding number in the middle to make sure it is not stripped down
+                test.testText = test.testText.substring(0, mid) + '%' + test.testText.substring(mid);
+                test.expected = test.testText;
+                // adding numbers around to make sure they are stripped down
+                test.testText = '#!' + test.testText + '^@';
+            });
+        },
+
+        tests: _.cloneDeep(testData)
+    }
 ];
 
 testCases.forEach(testCase => {
