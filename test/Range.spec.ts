45f4e9aa153338c3ca5fbb83d7c2d44ce09b718d
Sergei Dryganets <sergeyd@microsoft.com>
2018-03-06 20:05:21 -0800

    Range fixes. Invert is a flag now. Better tests

--- test/Range.spec.ts
+++ test/Range.spec.ts
@@ -1,53 +1,89 @@
 import { Range } from '../src/Range';
+const lowerLimit = 0;
+const upperLimit = 70;
+
+const range = new Range([0, 70], [[2, 2], [4, 5], [10, 15], [32, 40], [50, 50]]);
+let invertedRange = range.invert();
+
+function testIn(val: number, expected: boolean) {
+    expect(range.in(val)).toBe(expected);
+
+    const beyondTheLimit = val < lowerLimit || val > upperLimit;
+    const invertedExpected = beyondTheLimit ? expected : !expected;
+    expect(invertedRange.in(val)).toBe(invertedExpected);
+}
+
+function testOut(val: number, expected: boolean) {
+    expect(range.out(val)).toBe(expected);
+
+    const beyondTheLimit = val < lowerLimit || val > upperLimit;
+    const invertedExpected = beyondTheLimit ? expected : !expected;
+    expect(invertedRange.out(val)).toBe(invertedExpected);
+}
 
 describe('Range', () => {
-    let range: Range;
-    beforeAll(() => {
-        range = new Range([0, 70], [[2, 2], [4, 5], [10, 15], [32, 40], [50, 50]]).invert().invert();
-    });
 
     it('.in() left out of range', () => {
-        expect(false).toBe(range.in(1));
+        testIn(1, false);
     });
 
     it('.in() right out of range', () => {
-        expect(false).toBe(range.in(51));
+        testIn(51, false);
+    });
+
+    it('.in() right range beyond the limit', () => {
+        testIn(71, false);
+    });
+
+    it('.in() left beyond the limit', () => {
+        testIn(0, false);
     });
 
     it('.in() right range edge', () => {
-        expect(true).toBe(range.in(2));
+        testIn(2, true);
     });
 
-    it('.in() middle test', () => {
-        expect(true).toBe(range.in(10));
-        expect(true).toBe(range.in(15));
+    it('.in() middle in', () => {
+        testIn(33, true);
+    });
+
+    it('.in() middle out', () => {
+        testIn(18, false);
     });
 
     it('.in() left range inclusive', () => {
-        expect(true).toBe(range.in(5));
+        testIn(5, true);
     });
 
     it('.in() left range edge', () => {
-        expect(true).toBe(range.in(50));
+        testIn(50, true);
+    });
+
+    it('.out() middle out', () => {
+        testOut(18, true);
+    });
+
+    it('.out() middle in', () => {
+        testOut(33, false);
     });
 
     it('.out() left range beyond the limit', () => {
-        expect(false).toBe(range.out(-1));
+        testOut(-1, true);
     });
 
     it('.out() right range beyond the limit', () => {
-        expect(false).toBe(range.out(71));
+        testOut(71, true);
     });
 
-    it('.out() left out limit', () => {
-        expect(true).toBe(range.out(0));
+    it('.out() left beyond the limit', () => {
+        testOut(0, true);
     });
 
-    it('.out() left in limit', () => {
-        expect(false).toBe(range.out(2));
+    it('.out() left edge', () => {
+        testOut(2, false);
     });
 
-    it('.out() right in limit', () => {
-        expect(false).toBe(range.out(50));
+    it('.out() right edge', () => {
+        testOut(50, false);
     });
 });
